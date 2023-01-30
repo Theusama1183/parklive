7ad4517050a197e922e280a43e69ed386ec41425
Usama Adeel <Theusama1183>
2023-01-31 01:27:43 +0500

    ful

--- js/script.js
+++ js/script.js
@@ -16,3 +16,38 @@
     }
   }
 
+  
+  var currentTab = 0;
+  showTab(currentTab); 
+  
+  function showTab(n) {
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    if (n == (x.length - 1)) {
+      document.getElementById("nextBtn").innerHTML = "Ok";
+      
+    } else if (n == (x.length - 4)) {
+      document.getElementById("nextBtn").innerHTML = "Pay $40.00";
+    }
+    else if (n == (x.length - 2)) {
+      document.getElementById("nextBtn").innerHTML = "Extend";
+    }
+
+    else {
+      document.getElementById("nextBtn").innerHTML = "Proceed";
+    }
+    fixStepIndicator(n)
+  }
+  
+  function nextPrev(n) {
+    var x = document.getElementsByClassName("tab");
+    x[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+    if (currentTab >= x.length) {
+      document.getElementById("parking-steps").submit();
+      return false;
+    }
+    showTab(currentTab);
+  }
+  
+  
